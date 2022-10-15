ba560825d2b053bcb362d7435191998d4886a04b
rajdeep <rajdeeprajdeep03@gmail.com>
2022-10-15 23:44:50 +0530

    feat: navbar added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCBBfiTkcUju7br7vRDEjFhoc8VDEDOG8k",
+    authDomain: "challenge-aa1c7.firebaseapp.com",
+    projectId: "challenge-aa1c7",
+    storageBucket: "challenge-aa1c7.appspot.com",
+    messagingSenderId: "794109327744",
+    appId: "1:794109327744:web:027dba8882aa643a776965",
+    measurementId: "G-BFPVKHPVQ0"
+};
